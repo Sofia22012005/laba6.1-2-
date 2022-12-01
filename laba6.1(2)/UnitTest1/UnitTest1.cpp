fd0a6f12bdf62db0c1d02613b21234571715a85d
Sofia22012005 <114824939+Sofia22012005@users.noreply.github.com>
2022-12-01 21:41:14 +0200

    Add files via upload

---
+++ laba6.1(2)/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba6.1(2)/laba6.1(2).cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(number_test)
+		{
+			const int size = 5;
+			int b[size]{ -1, 2, 4, 5, 1 };
+			int num = number_of(b, size, 0, 0);
+			Assert::AreEqual(num, 4);
+		}
+
+		TEST_METHOD(sum_test)
+		{
+			const int size = 5;
+			int b[size]{ -1, 2, 4, 5, 1 };
+			int sum = sum_of(b, size, 0, 0);
+			Assert::AreEqual(sum, 10);
+		}
+
+		TEST_METHOD(zero_test)
+		{
+			const int size = 5;
+			int b[size]{ -1, 2, 4, 5, 1 };
+			zero(b, size, 0);
+			Assert::AreEqual(b[1], 0);
+			Assert::AreEqual(b[2], 0);
+		}
+	};
+}
